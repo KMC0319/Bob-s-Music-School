7c80f9177a0dc76a9d4466f13d6589de8a201dff
KMC0319 <31245129+KMC0319@users.noreply.github.com>
2019-04-23 02:33:51 +0900

    small

--- Bob's Music School/Assets/Scripts/Game/ResponseSound.cs
+++ Bob's Music School/Assets/Scripts/Game/ResponseSound.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 namespace Game {
     public class ResponseSound : MonoBehaviour {
         [SerializeField] private Player player;
-        [SerializeField] private AudioClip[] standardSounds;
+        [SerializeField] private AudioClip[] sounds;
         private AudioSource audioSource;
         
         private void Start() {
@@ -15,7 +15,7 @@ namespace Game {
         private void PlaySound(int num) {
             Debug.Log(num);
             if (player.HoldNotes[num % 4] != null) return;
-            audioSource.PlayOneShot(standardSounds[num]);
+            audioSource.PlayOneShot(sounds[num]);
         }
     }
 }
