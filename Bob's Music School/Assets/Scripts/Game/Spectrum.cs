b1aed6ce40402d6f632e682acf1f73550abb04f7
KMC0319 <31245129+KMC0319@users.noreply.github.com>
2019-04-24 04:57:01 +0900

    update spectrum

--- Bob's Music School/Assets/Scripts/Game/Spectrum.cs
+++ Bob's Music School/Assets/Scripts/Game/Spectrum.cs
@@ -3,9 +3,11 @@ using UnityEngine;
 namespace Game {
     public class Spectrum : MonoBehaviour {
         [SerializeField] private GameObject lineObject;
+        [SerializeField] private FFTWindow fftWindow;
         private const int NumberOfSamples = 256;
-        private float[] samples = new float[NumberOfSamples];
+        private float[] samples = new float[NumberOfSamples * 8];
         private GameObject[] lines = new GameObject[NumberOfSamples];
+
         private void Start() {
             for (var i = 0; i < NumberOfSamples; i++) {
                 var pos = (Vector2) transform.position + new Vector2(i, 0);
@@ -15,10 +17,14 @@ namespace Game {
         }
 
         private void Update() {
-            AudioListener.GetSpectrumData(samples, 0, FFTWindow.BlackmanHarris);
+            AudioListener.GetSpectrumData(samples, 0, fftWindow);
+            //AudioListener.GetOutputData(samples, 0);//音量で作るとき
             for (var i = 0; i < NumberOfSamples; i++) {
                 var scale = lines[i].transform.localScale;
-                scale.y = Mathf.Clamp(samples[i] * 2000, 1, 300);
+                scale.y = Mathf.Clamp(samples[i] * 4000, 1, 350);//横軸の変更度が均等なやつ
+                /*横軸の変更度を調整して、低・中音がたくさん表示される
+                var power = 3;
+                scale.y = Mathf.Clamp(samples[(int) (samples.Length * (float) (Mathf.Pow(i, power)) / (Mathf.Pow(NumberOfSamples, power)))] * 4000, 1, 350);*/
                 lines[i].transform.localScale = scale;
             }
         }
