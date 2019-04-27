20a6790ef18a08985fedf9f13c8564a1d768088d
Takoasihaisen <kouta0103@jcom.home.ne.jp>
2019-04-28 01:26:40 +0900

    タイトル画面を仮実装した
    まだ途中だがタイトルメニューを作る予定

---
+++ Bob's Music School/Assets/Scripts/UI/UiAnimationTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiAnimationTest : MonoBehaviour
+{
+    SpriteRenderer MainSpriteRenderer;
+
+    public GameObject GameObject;
+    public SpriteRenderer spRenderer;
+
+    // Use this for initialization
+    void Start()
+    {
+      
+
+    }
+
+    void TestUiAnimationEvent()
+    {
+        spRenderer.color = new Color(1,1,1,1);
+
+    }
+}
